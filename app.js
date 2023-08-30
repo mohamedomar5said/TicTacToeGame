574bde09c3364d79659c2de57de3b4301a51d5a3
mohamedomar <123811850+mohamedomar5said@users.noreply.github.com>
2023-08-30 14:06:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
-
 const playerDisplay = document.querySelector('.display-player');
 const tiles = [...document.querySelectorAll('.tile')];
 const announcer = document.querySelector('.announcer');
 const resetBtn = document.querySelector('#reset');
 
-
-let turn = 'x';
+let turn = 'X';
 
 tiles.forEach(tile => {
 
@@ -15,13 +13,13 @@ tiles.forEach(tile => {
 
 function game(event) {
    let element = event.target;
-   if (turn === 'x' && element.innerHTML == '') {
-      element.innerHTML = 'x';
-      turn = 'o';
+   if (turn === 'X' && element.innerHTML == '') {
+      element.innerHTML = 'X';
+      turn = 'O';
       playerDisplay.innerHTML = 'O';
-   } else if (turn === 'o' && element.innerHTML == '') {
-      element.innerHTML = 'o';
-      turn = 'x';
+   } else if (turn === 'O' && element.innerHTML == '') {
+      element.innerHTML = 'O';
+      turn = 'X';
       playerDisplay.innerHTML = 'X';
    }
    compare();
@@ -29,7 +27,6 @@ function game(event) {
 
 function compare() {
 
-   let isGameActive = true;
 
    let count = 0;
 
@@ -40,122 +37,53 @@ function compare() {
 
    }
 
-   if (isGameActive) {
+   function won(num1, num2, num3) {
+      announcer.innerHTML = `${tiles[num1].innerHTML} has won`;
+      document.getElementById(`item${num1 + 1}`).style.backgroundColor = 'rgb(255 0 0)';
+      document.getElementById(`item${num2 + 1}`).style.backgroundColor = 'rgb(255 0 0)';
+      document.getElementById(`item${num3 + 1}`).style.backgroundColor = 'rgb(255 0 0)';
+      stopTheGame();
+   }
 
-      if (tiles[0].innerHTML === 'x' && tiles[1].innerHTML === 'x' && tiles[2].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-         isGameActive = false;
-         stopTheGame();
-
-      } else if (tiles[0].innerHTML === 'o' && tiles[1].innerHTML === 'o' && tiles[2].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-      if (tiles[3].innerHTML === 'x' && tiles[4].innerHTML === 'x' && tiles[5].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[3].innerHTML === 'o' && tiles[4].innerHTML === 'o' && tiles[5].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-      if (tiles[6].innerHTML === 'x' && tiles[7].innerHTML === 'x' && tiles[8].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[6].innerHTML === 'o' && tiles[7].innerHTML === 'o' && tiles[8].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-      if (tiles[0].innerHTML === 'x' && tiles[3].innerHTML === 'x' && tiles[6].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[0].innerHTML === 'o' && tiles[3].innerHTML === 'o' && tiles[6].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-
-      if (tiles[1].innerHTML === 'x' && tiles[4].innerHTML === 'x' && tiles[7].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[1].innerHTML === 'o' && tiles[4].innerHTML === 'o' && tiles[7].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-      if (tiles[2].innerHTML === 'x' && tiles[5].innerHTML === 'x' && tiles[8].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[2].innerHTML === 'o' && tiles[5].innerHTML === 'o' && tiles[8].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-      if (tiles[0].innerHTML === 'x' && tiles[4].innerHTML === 'x' && tiles[8].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[0].innerHTML === 'o' && tiles[4].innerHTML === 'o' && tiles[8].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
-
-      if (tiles[2].innerHTML === 'x' && tiles[4].innerHTML === 'x' && tiles[6].innerHTML === 'x') {
-         announcer.innerHTML = 'X has won';
-
-         stopTheGame();
-
-      } else if (tiles[2].innerHTML === 'o' && tiles[4].innerHTML === 'o' && tiles[6].innerHTML === 'o') {
-
-         announcer.innerHTML = 'O has won';
-
-         stopTheGame();
-
-      }
+   if (tiles[0].innerHTML == tiles[1].innerHTML && tiles[1].innerHTML == tiles[2].innerHTML && tiles[2].innerHTML != '') {
+      won(0, 1, 2);
 
    }
-   if (count === 9) {
+   if (tiles[3].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[5].innerHTML && tiles[5].innerHTML != '') {
+      won(3, 4, 5);
+
+   }
+   if (tiles[6].innerHTML == tiles[7].innerHTML && tiles[7].innerHTML == tiles[8].innerHTML && tiles[8].innerHTML != '') {
+      won(6, 7, 8);
+   }
+
+   if (tiles[0].innerHTML == tiles[3].innerHTML && tiles[3].innerHTML == tiles[6].innerHTML && tiles[6].innerHTML != '') {
+      won(0, 3, 6);
+   }
+
+   if (tiles[1].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[7].innerHTML && tiles[7].innerHTML != '') {
+      won(1, 4, 7);
+   }
+
+   if (tiles[2].innerHTML == tiles[5].innerHTML && tiles[5].innerHTML == tiles[8].innerHTML && tiles[8].innerHTML != '') {
+      won(2, 5, 8);
+   }
+
+   if (tiles[0].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[8].innerHTML && tiles[8].innerHTML != '') {
+      won(0, 4, 8);
+   }
+
+   if (tiles[2].innerHTML == tiles[4].innerHTML && tiles[4].innerHTML == tiles[6].innerHTML && tiles[6].innerHTML != '') {
+      won(2, 4, 6);
+   }
+
+   if (count === 9 && announcer.innerHTML == '') {
       announcer.innerHTML = "It's a tie";
       stopTheGame();
 
    }
 
 }
-
 
 function stopTheGame() {
    tiles.forEach((tile) => {
